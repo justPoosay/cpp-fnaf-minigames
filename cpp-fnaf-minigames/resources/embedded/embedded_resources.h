4eb8c883ebbe122199d6f10434edfc04c276805b
justPoosai <u50_oscsta_lbn@technischools.com>
2025-05-13 10:57:38 +0200

    push to main

---
+++ cpp-fnaf-minigames/resources/embedded/embedded_resources.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Auto-generated resource header
+#include <stddef.h>
+
+typedef struct {
+    const char* name;
+    const unsigned char* data;
+    unsigned int length;
+} ResourceEntry;
+
+static const ResourceEntry embedded_resources[] = {
+};
+static const size_t embedded_resource_count = 0;
